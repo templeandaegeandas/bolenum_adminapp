140b693093fc45a01fd487a69bf294c61092f3d0
vishal910 <vishal.kumar@oodlestechnologies.com>
2017-10-17 16:03:22 +0530

    erc20 add complete

--- src/app/pages/addNewErc20Token/entity/erc20Token.ts
+++ src/app/pages/addNewErc20Token/entity/erc20Token.ts
@@ -2,5 +2,9 @@ export class AddNewErc20Token {
   contractAddress: String;
   walletAddress: String;
   currency: any;
- 
+}
+
+export class Currency {
+  currencyName: String;
+  currencyAbbreviation: String;
 }
