31157a62ec2c4a5df403cf4ddfeb7afbe15ea456
Himanshu <himanshu.kumar@oodlestechnologies.com>
2017-11-20 19:57:25 +0530

    interate api to show latest order placed by user

--- src/app/pages/dashboard/dashboard.service.ts
+++ src/app/pages/dashboard/dashboard.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '../../app.client.interceptor';
 import 'rxjs/add/operator/map';
-
 
 @Injectable()
 export class DashBoardService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  pageNumber: any;
+  pageSize: any;
+  sortBy: any;
+  sortOrder: any;
+
+  getListOfLatestOrders(currentPage: number, pageSize: number, sortBy: String, sortOrder: String) {
+  this.pageNumber = currentPage - 1;
+  return this.http.get('/api/v1/admin/latest/order/list?pageNumber='
+    + this.pageNumber + '&pageSize='
+    + pageSize + '&sortBy='
+    + sortBy + '&sortOrder='
+    + sortOrder)
+    .map(res => res.json());
+  }
 
 }
