7f5de6e8c2196939d2c13487542cb0f21d9c9703
Himanshu <himanshu.kumar@oodlestechnologies.com>
2017-12-08 20:36:40 +0530

    integrated newletter in feed

--- src/app/pages/dashboard/feed/feed.component.ts
+++ src/app/pages/dashboard/feed/feed.component.ts
@@ -1,28 +1,40 @@
-import {Component} from '@angular/core';
-
-import {FeedService} from './feed.service';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'toastr-ng2';
+import { FeedService } from './feed.service';
 
 @Component({
   selector: 'feed',
   templateUrl: './feed.html',
-  styleUrls: ['./feed.scss']
+  styleUrls: ['./feed.scss'],
+    providers: [FeedService] ,
 })
-export class Feed {
 
-  public feed:Array<Object>;
+export class Feed implements OnInit {
 
-  constructor(private _feedService:FeedService) {
+  subscribedUserlist: any;
+  isLoading: any;
+  hasBlur: any;
+  feed: any;
+  listLength: any;
+  constructor(private feedService: FeedService, private toastrService: ToastrService) {
   }
 
   ngOnInit() {
-    this._loadFeed();
+    this.getListOfSubscribedUser();
   }
 
-  expandMessage (message){
+  expandMessage(message) {
     message.expanded = !message.expanded;
   }
 
-  private _loadFeed() {
-    this.feed = this._feedService.getData();
+  getListOfSubscribedUser() {
+    this.isLoading = true;
+    this.hasBlur = true;
+    this.feedService.subscribedUserList(1, 10, 'createdOn', 'desc').subscribe(success => {
+      this.isLoading = false;
+      this.hasBlur = false;
+      this.feed = success.data.content;
+      this.listLength = this.feed.length;
+    });
   }
 }
