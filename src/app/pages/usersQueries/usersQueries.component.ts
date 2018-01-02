44a629e510dc0c7da8fedbf69cf02e1f999a1e25
swatirahangdale <swati.rahangdale@oodlestechnologies.com>
2018-01-02 17:37:55 +0530

    worked on userqueries page

--- src/app/pages/usersQueries/usersQueries.component.ts
+++ src/app/pages/usersQueries/usersQueries.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'toastr-ng2';
 import { UsersQueriesService } from './usersQueries.service';
 import { Router } from '@angular/router';
 
@@ -6,31 +7,41 @@ import { Router } from '@angular/router';
   selector: 'usersQueries',
   styleUrls: [('./usersQueries.scss')],
   templateUrl: './usersQueries.html',
-  providers: [UsersQueriesService]
+  providers: [UsersQueriesService],
 })
-export class UsersQueries {
+export class UsersQueries implements OnInit {
 
-   data;
-    filterQuery = '';
-    rowsOnPage = 10;
-    sortBy = 'email';
-    sortOrder = 'asc';
+  subscribedUserlist: any;
+  isLoading: any;
+  hasBlur: any;
+  listLength: any;
+   usersQueries: any;
 
-    constructor(private service: UsersQueriesService, private router: Router) {
-    this.service.getDataTable().then((data) => {
-      this.data = data;
+  constructor(private usersQueriesService: UsersQueriesService, private router: Router, private toastrService: ToastrService) {
+    
+  }
+    ngOnInit() {
+    this.getListOfSubscribedUser();
+  }
+
+  expandMessage(message) {
+    message.expanded = !message.expanded;
+  }
+
+  getListOfSubscribedUser() {
+    this.isLoading = true;
+    this.hasBlur = true;
+    this.usersQueriesService.subscribedUserList(1, 10, 'createdOn', 'desc').subscribe(success => {
+      this.isLoading = false;
+      this.hasBlur = false;
+      this.usersQueries = success.data.content;
+      this.listLength = this.usersQueries.length;
     });
   }
-
-    toInt(num: string) {
-        return +num;
-    }
-
-    sortByWordLength = (a: any) => {
-        return a.city.length;
-    }
-  navigaeToReply()
-  {
-    this.router.navigate(['/pages/reply'])
-  }
 }
+
+  // navigaeToReply()
+  // {
+  //   this.router.navigate(['/pages/reply']);
+  // }
+
