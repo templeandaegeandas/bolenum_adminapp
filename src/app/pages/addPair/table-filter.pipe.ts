9276da68f987027c5dbcd249c2003044ebccf815
pankajoodles <pankaj.thakur@oodlestechnologies.com>
2018-01-05 17:28:10 +0530

    working on add pair module to filter the pair using pipe

--- src/app/pages/addPair/table-filter.pipe.ts
+++ src/app/pages/addPair/table-filter.pipe.ts
@@ -8,16 +8,23 @@ export class TableFilterPipe implements PipeTransform {
 
     transform(value: any[], args: string): any {
 
+        args = args.toLowerCase() || args.toUpperCase();
+
         if (!args[0]) {
-            
-            return value; 
+
+            return value;
 
         }
-     
-        return value.filter((item ) => {
 
-            return item.pairName.includes(args) || item.toCurrency[0].currencyName.includes(args) ;
+        return value.filter((item) => {
+
+            return item.pairName.toLowerCase().includes(args)
+                || item.pairName.toUpperCase().includes(args)
+                || item.toCurrency[0].currencyName.toLowerCase().includes(args)
+                || item.toCurrency[0].currencyName.toUpperCase().includes(args)
+                || item.pairedCurrency[0].currencyName.toLowerCase().includes(args)
+                || item.pairedCurrency[0].currencyName.toUpperCase().includes(args);
         });
-     }
+    }
 }
 
